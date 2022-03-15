94ebb94fceee4f7bf8b4d56f8e44708e58299d32
Jean Charl Greeff <44994396+greeffjean@users.noreply.github.com>
2022-03-15 10:54:38 +0200

    Initial commit

---
+++ components/Store.js
@@ -0,0 +1,24 @@
+import { ListGroup } from 'react-bootstrap';
+import { useState } from 'react';
+
+function Store() {
+    const [ count, setCount ] = useState(0);
+
+    return (
+      <>
+        <ListGroup>
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </>
+    );
+  }
+  
+  export default Store;
+  
---
+++ pages/store/index.js
@@ -0,0 +1,11 @@
+import StoreList from '../../components/Store';
+
+function Store(){
+  return (
+    <>
+      <StoreList />
+    </>
+  );
+}
+
+export default Store;
